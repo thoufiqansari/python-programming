a2edf8a8f13ca2b80f7f7a9d88805b6f800e057e
Thoufiq Ansari KS <36226852+thoufiqansari@users.noreply.github.com>
2018-09-24 14:39:25 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,13 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+ch=input()
+if ch>='a' and ch<='z':
+    print("Alphabet")
+else:
+    print("No")
