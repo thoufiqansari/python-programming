be8ad4a2fd4dd272e8b7dafee9ed758bf783177a
Thoufiq Ansari KS <36226852+thoufiqansari@users.noreply.github.com>
2018-09-24 14:15:24 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,13 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+ch=input()
+if(ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u"):
+    print("Vowel")
+else:
+    print("invalid")
