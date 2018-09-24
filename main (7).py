4c86afa887b78fc5ccb85c23d097ac8f00ef32ec
Thoufiq Ansari KS <36226852+thoufiqansari@users.noreply.github.com>
2018-09-24 15:24:08 +0530

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,17 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+a=int(input())
+b=int(input())
+c=int(input())
+if a > b and a > c:
+    print(a)
+elif b > c and b > a:
+    print(b)
+else:
+    print(c)
