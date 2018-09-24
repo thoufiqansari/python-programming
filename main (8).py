d41c7e6fb228baa39db58554b50122629e7fd7b1
Thoufiq Ansari KS <36226852+thoufiqansari@users.noreply.github.com>
2018-09-24 15:27:39 +0530

    Add files via upload

---
+++ main (8).py
@@ -0,0 +1,12 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+a=int(input())
+b=int(input())
+c=int(input())
+print(max(a,b,c))
