cc27e61bf4c5e344ba768017ee0e3f3ec8722c6b
Thoufiq Ansari KS <36226852+thoufiqansari@users.noreply.github.com>
2018-09-24 13:51:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+num=int(input("enter the number:"))
+if num%2==0:
+    print("Even")
+elif num%2!=0:
+    print("Odd")
+else:
+    print("invalid")
