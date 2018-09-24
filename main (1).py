d637c11468f29532035c29a47fd4d0812707ed6b
Thoufiq Ansari KS <36226852+thoufiqansari@users.noreply.github.com>
2018-09-24 10:48:10 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+num=int(input("enter your number:"))
+if num==3:
+    print("possitive")
+elif num==0:
+    print("zero")
+else:
+    print("negative")
